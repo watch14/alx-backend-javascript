d9ee695db7bbc4cc28635b39fbe7e6e0341182a8
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-22 18:33:51 +0100

    fast_push

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,10 +2,10 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-    const fullBudget = {
-      ...budget,
-      getIncomeInDollars: income => `$${income}`,
-      getIncomeInEuros: income => `${income} euros`,
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: income => `$${income}`,
+    getIncomeInEuros: income => `${income} euros`
   };
 
   return fullBudget;
