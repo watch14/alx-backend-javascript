8af30a8d3825fbd3787769f9018318a10b51e014
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-22 19:04:58 +0100

    fast_push

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,16 @@
 export default function createIteratorObject(report) {
   function* iterateEmployees() {
     for (const department in report.allEmployees) {
-      const employees = report.allEmployees[department];
-      for (const employee of employees) {
-        yield employee;
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
+        }
       }
     }
   }
 
   return {
-    [Symbol.iterator]: iterateEmployees
+    [Symbol.iterator]: iterateEmployees,
   };
 }
