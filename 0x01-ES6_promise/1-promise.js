f8b619fddaba2fc891bb0b0b26f76ac28bd35e2f
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-23 16:26:24 +0100

    fast_push

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,9 @@
-export
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
