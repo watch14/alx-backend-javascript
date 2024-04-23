f00587cd337e97ee27ea775a11397fd8b16ce658
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-23 18:28:23 +0100

    fast_push

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
