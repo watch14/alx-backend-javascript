4c8987630e325991679096c7957ffee33bb81b96
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-23 17:50:29 +0100

    fast_push

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const pic = uploadPhoto(fileName);
-
-  return Promise.allSettled([user, pic]).then((resualt) => {
-    resualt.map((val) => ({
-      status: val.status,
-      value: val.status === 'fulfilled' ? val.value : String(val.reson),
-    }));
-  });
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
