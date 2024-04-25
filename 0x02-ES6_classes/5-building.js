fb51d1890c52bd032368942f20693d0607aac55e
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-26 00:49:49 +0100

    fast_push

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,11 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      const props = Object.getOwnPropertyNames(this.constructor.prototype);
+      if (!props.find((e) => e === 'evacuationWarningMessage')) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
     this._sqft = sqft;
   }
 
@@ -7,7 +13,10 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(sqft) {
+    if ((typeof sqft !== 'number') && (sqft instanceof Number)) {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 }
