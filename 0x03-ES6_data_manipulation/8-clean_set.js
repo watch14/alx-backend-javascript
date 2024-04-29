a13fb3fefaf90d118b28e777c621ba840ad3134c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-04-29 20:11:48 +0100

    fast_push

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,4 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  return filteredValues.join('-').replace(new RegExp(`^${startString}`), '');
+}
