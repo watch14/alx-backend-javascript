fbb494d67ee2993e7306121925b0470abbfe96e1
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-01 23:23:36 +0100

    fast_push

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,42 +1,43 @@
 interface DirectorInterface {
-  workFromHome: string;
-  getCoffeeBreak: string;
-  workDirectorTasks: string;
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
-  workFromHome: string;
-  getCoffeeBreak: string;
-  workDirectorTasks: string;
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
 }
 
 export class Director implements DirectorInterface {
   workFromHome() {
-    return 'Working from home'
+    return 'Working from home';
   }
-  getToWork() {
-    return 'Getting a coffee break'
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
   }
   workDirectorTasks() {
-    return 'Getting to director tasks'
+    return 'Getting to director tasks';
   }
 }
 
 export class Teacher implements TeacherInterface {
   workFromHome() {
-    return 'Cannot work from home'
+    return 'Cannot work from home';
   }
   getCoffeeBreak() {
-    return 'Cannot have a break'
+    return 'Cannot have a break';
   }
   workTeacherTasks() {
-    return 'Getting to work'
+    return 'Getting to work';
   }
 }
 
-export function createEmployee(salary: (number | string)): (Director | Teacher) {
+export function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) {
-    return new Teacher()
+    return new Teacher();
   }
-  return new Director()
+  return new Director();
 }
+
