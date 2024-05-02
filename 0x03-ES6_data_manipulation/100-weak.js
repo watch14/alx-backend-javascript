b384c802229fd8e767a0bb2ba1ccb794efc146ed
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-02 21:40:18 +0100

    fast_push

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,20 @@
 const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
+export { weakMap };
 
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  return count;
 }
 
-export { queryAPI, weakMap };
+export default queryAPI;
