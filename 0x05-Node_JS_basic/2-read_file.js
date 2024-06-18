04c654c443f14607337016c603604d488e0e310f
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-18 18:32:21 +0100

    t-2 async

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const students = data.split('\n').filter(line => line !== '');
+    const count = students.length - 1;
+    console.log(`Number of students: ${count}`);
+
+    const fields = {};
+    students.slice(1).forEach(line => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    Object.keys(fields).forEach(field => {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
