4ee2b2f0bb864dacd9ffd24ae759c7a89e522991
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-20 19:28:42 +0100

    t 9 + 8 ?

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -10,4 +10,13 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+app.get('/cart/:id', (req, res) => {
+  res.status(404).send('Invalid cart ID. Cart ID must be a number.');
+});
+
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -15,28 +15,13 @@ describe('Index page', function () {
   });
   it('check correct content', function (done) {
     request(options, function (err, res, body) {
-	    expect(body).to.equal('Welcome to the payment system');
+	    expect(body).to.contain('Welcome to the payment system');
 	    done();
     });
   });
-});
-
-describe('Cart page', function () {
-  it('check correct status code for correct url', function (done) {
-    request.get('http://localhost:7865/cart/12', function (err, res, body) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-    });
-  });
-  it('check correct content for correct url', function (done) {
-    request.get('http://localhost:7865/cart/12', function (err, res, body) {
-	    expect(body).to.contain('Payment methods for cart 12');
-	    done();
-    });
-  });
-  it('check correct status code for incorrect url', function (done) {
-    request.get('http://localhost:7865/cart/kim', function (err, res, body) {
-	    expect(res.statusCode).to.equal(404);
+  it('check correct content length', function (done) {
+    request(options, function (err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
 	    done();
     });
   });
