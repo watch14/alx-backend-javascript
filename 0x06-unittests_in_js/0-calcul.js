b24147e428eb37ea87f357e5bf57d238a9b3f019
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-20 17:56:03 +0100

    t0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
-function calcul (a, b) {
+function calculateNumber (a, b) {
   const ra = Math.round(a);
   const rb = Math.round(b);
   return ra + rb;
 }
 
-module.exports = calcul;
+module.exports = calculateNumber;
