d240e696983d452e127db5f5bb1e37d38e197c97
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-20 18:07:20 +0100

    t0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
+describe('calculateNumber', function () {
   it('should return 4 when inputs are 1 and 3', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
@@ -16,5 +16,25 @@ describe('calculateNumber', () => {
 
   it('should return 6 when inputs are 1.5 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when inputs are -0.1 and 0.1', function () {
+    assert.strictEqual(calculateNumber(-0.1, 0.1), 0);
+  });
+
+  it('should return 0 when inputs are 0 and 0', function () {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return 2 when inputs are 0.5 and 0.5', function () {
+    assert.strictEqual(calculateNumber(0.5, 0.5), 2);
+  });
+
+  it('should return 3 when inputs are 1.4 and 1.5', function () {
+    assert.strictEqual(calculateNumber(1.4, 1.5), 3);
+  });
+
+  it('should return -1 when inputs are -1.4 and -0.5', function () {
+    assert.strictEqual(calculateNumber(-1.4, -0.5), -1);
   });
 });
